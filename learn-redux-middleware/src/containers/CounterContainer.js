18642840ac806dcc71c0a312649c7360890f2f0f
taulmax <taulmax@naver.com>
2021-01-19 17:14:00 +0900

    와 다공부했다!
    와 다공부했다!

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import Counter from "../components/Counter";
-import { increase, decrease } from "../modules/counter";
+import { increaseAsync, decreaseAsync } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+const CounterContainer = ({ number, increaseAsync, decreaseAsync }) => {
+  return <Counter number={number} onIncrease={increaseAsync} onDecrease={decreaseAsync} />;
 };
 
 export default connect(
@@ -12,7 +12,7 @@ export default connect(
     number: state.counter,
   }),
   {
-    increase,
-    decrease,
+    increaseAsync,
+    decreaseAsync,
   }
 )(CounterContainer);
--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -7,9 +7,10 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 // import loggerMiddleware from "./lib/loggerMiddleware";
 import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
